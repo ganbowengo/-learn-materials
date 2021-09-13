f185ff7cb5d1c8d265c10c46935751bdc98fdd1c
ganbowen <ganbowen@baiwang.com>
2021-09-13 09:51:44 +0800

    js优化

---
+++ 3. js优化/eg/1. memory.js
@@ -0,0 +1,15 @@
+/*
+ * @Author       : ganbowen
+ * @Date         : 2021-09-12 20:43:25
+ * @LastEditors  : ganbowen
+ * @LastEditTime : 2021-09-12 20:43:41
+ * @Descripttion : memory-management
+ */
+
+// 申请
+let obj = {}
+// 使用
+obj.name = 'tom'
+// 释放
+obj = null
+
